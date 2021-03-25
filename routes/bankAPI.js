cddcd049dbe84d8bfea26cd970b1cf48e8304d96
Jürgen Sepp <jurgen.sepp@khk.ee>
2021-03-25 17:04:00 +0200

    changes to fit barbank's api

--- routes/bankAPI.js
+++ routes/bankAPI.js
@@ -1,88 +1,47 @@
 let express = require("express");
-let router= express.Router();
+let router = express.Router();
+const base = 'https://barbank.diarainfra.com';
 
-function genToken() {
-    let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    let string_length = 16;
-    let randomstring = '';
-    for (let i=0; i<string_length; i++) {
-        let rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
+function send(res, source, method, path, data, token) {
+    const fetch = process.browser ? window.fetch : require('node-fetch').default;
+
+    const opts = { method, headers: {}};
+
+    if (data) {
+        opts.headers['Content-Type'] = 'application/json';
+        opts.body = JSON.stringify(data);
     }
-    return randomstring;
+
+    if (token) {
+        opts.headers['Authorization'] = 'Bearer '+token;
+    }
+    fetch(base+"/"+path, opts)
+        .then(r => r.text())
+        .then(json => Object.defineProperty(JSON.parse(json), 'source', {'value': source, enumerable: true}))
+        .then(json => res.send(json))
 }
 
-let accounts =  {}
-let tokens = {}
-
 router.post("/login", function(req, res) {
-    const username = req.body.user.toLowerCase();
-    if(accounts.hasOwnProperty(username) && accounts[username].pass === req.body.pass) {
-        const userToken = genToken();
-        tokens[username].string = userToken; // set token
-        tokens[username].life = Date.now()+3.6e6; //3600 second lifespan
-        res.json({status: 111,
-            user: accounts[username].user,
-            string: userToken
-        }); // success
-        //res.json({name: accounts[username].user, token: userToken})
-    } else if(!accounts.hasOwnProperty(username)) {
-        res.json({status: 211}) // user does not exist
-    } else {
-        res.json({status: 212}) // incorrect password
-    }
+    send(res, 'login', 'POST', 'sessions', {
+        "username": req.body.user,
+        "password": req.body.pass
+    })
 })
 
 router.post("/create-account", function(req, res) {
-    const username = req.body.user.toLowerCase();
-    if(!accounts.hasOwnProperty(username)) {
-        accounts[username] = req.body;
-        tokens[username] = {string: "", life: 0} // initialize token object (so checks don't cause errors)
-        res.json({
-            status: 121,
-            user: accounts[username].user
-        }); // success
-    } else {
-        res.json({status: 221}); // user exists
-    }
+    send(res, 'create-account', 'POST', 'users', {
+        "name": req.body.name,
+        "username": req.body.user,
+        "password": req.body.pass
+    })
 })
 
 router.post("/check-token", function(req, res) {
-    const username = req.body.user.toLowerCase();
-    if(!tokens.hasOwnProperty(username)) {
-        return res.json({status: 231}); // account/token doesn't exist (server restart while logged in?)
-    }
-    if(tokens[username].string === req.body.string && tokens[username].life > Date.now()) {
-        res.json({
-            status: 131,
-            user: accounts[username].user
-        }); // success
-    } else if(tokens[username].life < Date.now()) {
-        res.json({
-            status: 232,
-        }); // session expired
-    } else {
-        res.json({
-            status: 233,
-            receivedString: JSON.stringify(req.body),
-            expectedString: tokens[username].string
-        }); // invalid login token (server restart? or bugs)
-    }
+    send(res, 'check-token', 'GET', 'users/current', undefined, req.body.token);
 })
 
 router.post("/logout", function(req, res) {
-    const username = req.body.user.toLowerCase();
-    if(!tokens.hasOwnProperty(username)) {
-        return res.json({status: 241}); // account/token doesn't exist (server restart while logged in?)
-    }
-    if(tokens[username].string === req.body.string) { //must check token so fraudulent requests can't log out random people
-        tokens[username] = {string: "", life: 0}
-        res.json({
-            status: 141
-        }) // success
-    } else {
-        res.json({status: 242}) // invalid token (somehow)
-    }
+
 })
 
 module.exports = router;
